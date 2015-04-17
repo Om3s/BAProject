c0db5f5ff481a7912fb5a178c79d030c0f2721e6
Jonas Ostergaard <jonas.ostergaard@uni-konstanz.de>
2015-04-17 16:48:37 +0200

    - libs folder to .gitognore added - dataimport with opencvs implemented - first steps with geotools(testing)

--- BAProject_JOstergaard/src/mvc/main/Main.java
+++ BAProject_JOstergaard/src/mvc/main/Main.java
@@ -1,18 +1,49 @@
 package mvc.main;
 
-import mvc.model.CaseReport;
+import java.io.File;
+import java.io.IOException;
+
+import org.geotools.data.FileDataStore;
+import org.geotools.data.FileDataStoreFinder;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.map.FeatureLayer;
+import org.geotools.map.Layer;
+import org.geotools.map.MapContent;
+import org.geotools.styling.SLD;
+import org.geotools.styling.Style;
+import org.geotools.swing.JMapFrame;
+import org.geotools.swing.data.JFileDataStoreChooser;
+
 import mvc.model.CrimeCaseDatabase;
 import mvc.view.Mainframe;
 
 public class Main {
 
-	public static void main(String[] args) {
-		CrimeCaseDatabase dataBase = new CrimeCaseDatabase("dat\\testData.csv");
-		
+	public static void main(String[] args) throws IOException {
+//		CrimeCaseDatabase dataBase = new CrimeCaseDatabase("dat\\Case_Data_from_San_Francisco_311__SF311_.csv");
+//		
 //		Mainframe mFrame = new Mainframe();
 //		mFrame.setVisible(true);
 		
-		
+		// display a data store file chooser dialog for shapefiles
+        File file = JFileDataStoreChooser.showOpenFile("shp", null);
+        if (file == null) {
+            return;
+        }
+
+        FileDataStore store = FileDataStoreFinder.getDataStore(file);
+        SimpleFeatureSource featureSource = store.getFeatureSource();
+
+        // Create a map content and add our shapefile to it
+        MapContent map = new MapContent();
+        map.setTitle("Quickstart");
+        
+        Style style = SLD.createSimpleStyle(featureSource.getSchema());
+        Layer layer = new FeatureLayer(featureSource, style);
+        map.addLayer(layer);
+
+        // Now display the map
+        JMapFrame.showMap(map);
 	}
 
 }
