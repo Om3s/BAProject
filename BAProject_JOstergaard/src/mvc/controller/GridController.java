c5b41c4683bd6aab1a8df2e54c0a752e8a92a02a
Jonas Ostergaard <omesostergaard@msn.com>
2016-04-26 21:27:34 +0200

    fixed a bug and implemented the option of filtering the minMaxRelativeAmounts

--- BAProject_JOstergaard/src/mvc/controller/GridController.java
+++ BAProject_JOstergaard/src/mvc/controller/GridController.java
@@ -24,7 +24,7 @@ public class GridController {
 	public void analyze(int xResolution, int yResolution, Date fromDate, Date toDate, Coordinate topLeft, Coordinate botRight){
 		Gridmodel.getInstance().init(xResolution, yResolution, topLeft, botRight);
 		long dateInterval = toDate.getTime() - fromDate.getTime(); //interval is current date selection
-		int intervalAmount = 3; //TODO intervalAmount hardcoded
+		int intervalAmount = 2; //TODO intervalAmount hardcoded
 		Date fDate = fromDate;
 		Date tDate = toDate;
 		Gridmodel.getInstance().getData().setDataMatrix(this.countGridOccurenciesFromTo(fDate, tDate));
@@ -41,47 +41,88 @@ public class GridController {
 			newData.setDataMatrix(this.countGridOccurenciesFromTo(fDate, tDate));
 			this.pastGridModelData.add(newData);
 		}
-		int minNegValue = Integer.MIN_VALUE, maxNegValue = Integer.MAX_VALUE, minPosValue = Integer.MAX_VALUE,maxPosValue = Integer.MIN_VALUE;
+		
+		this.matrixCalculations();
+	}
+	
+	private void matrixCalculations() {
+		boolean minMaxByFilters = false;
+		double	minNegValue,maxNegValue,minPosValue,maxPosValue;
+		if(minMaxByFilters){
+			// min max determined by filters: TODO filternumbers are hardcoded atm
+			minNegValue = 0.0;
+			maxNegValue = -0.2;
+			minPosValue = 0.0;
+			maxPosValue = 0.2;
+		} else {
+			// min max of data:
+			minNegValue = Double.MIN_VALUE;
+			maxNegValue = Double.MAX_VALUE;
+			minPosValue = Double.MAX_VALUE;
+			maxPosValue = Double.MIN_VALUE;
+		}
 		int[][] pastDataAverage = this.calculateWeightedAverage(this.pastGridModelData.toArray(new GridModelData[0]));
 		int[][] absoluteDataDifference = new int[Gridmodel.getInstance().getXResolution()][Gridmodel.getInstance().getYResolution()];
 		double[][] relativeDataDifference = new double[Gridmodel.getInstance().getXResolution()][Gridmodel.getInstance().getYResolution()];
-		for(int y=0; y<Gridmodel.getInstance().getYResolution(); y++){
+		for(int y=0; y<Gridmodel.getInstance().getYResolution(); y++){ //Fill absolute- and relativeDifferenceMatrices:
 			for(int x=0; x<Gridmodel.getInstance().getXResolution();x++){
 				absoluteDataDifference[x][y] = Gridmodel.getInstance().getData().getDataMatrix()[x][y] - pastDataAverage[x][y];
-				if(absoluteDataDifference[x][y] >= 0){ //Positive:
-					if(absoluteDataDifference[x][y] > maxPosValue){
-						maxPosValue = absoluteDataDifference[x][y];
-					}
-					if(absoluteDataDifference[x][y] < minPosValue){
-						minPosValue = absoluteDataDifference[x][y];
-					}
-				} else { //Negative:
-					if(absoluteDataDifference[x][y] > minNegValue){
-						minNegValue = absoluteDataDifference[x][y];
-					}
-					if(absoluteDataDifference[x][y] < maxNegValue){
-						maxNegValue = absoluteDataDifference[x][y];
-					}
-				}
 				if(absoluteDataDifference[x][y] == 0){
 					relativeDataDifference[x][y] = 0.0;		
 				} else {
 					relativeDataDifference[x][y] = (double)absoluteDataDifference[x][y] / (double)Gridmodel.getInstance().getData().getDataMatrix()[x][y];
 				}
+				if(!minMaxByFilters){
+					//save the positive/negative min and max values from the relative matrix
+					if(relativeDataDifference[x][y] >= 0){ //Positive:
+						if(relativeDataDifference[x][y] > maxPosValue){
+							maxPosValue = relativeDataDifference[x][y];
+						}
+						if(relativeDataDifference[x][y] < minPosValue){
+							minPosValue = relativeDataDifference[x][y];
+						}
+					} else { //Negative:
+						if(relativeDataDifference[x][y] > minNegValue){
+							minNegValue = relativeDataDifference[x][y];
+						}
+						if(relativeDataDifference[x][y] < maxNegValue){
+							maxNegValue = relativeDataDifference[x][y];
+						}
+					}
+				}
 			}
 			System.out.println("");
 		}
 		double[][] normalizedRelativeMatrix = new double[Gridmodel.getInstance().getXResolution()][Gridmodel.getInstance().getYResolution()];
-		for(int y=0; y<Gridmodel.getInstance().getYResolution(); y++){
+		for(int y=0; y<Gridmodel.getInstance().getYResolution(); y++){ //fill the normalized RelativeMatrix
 			for(int x=0; x<Gridmodel.getInstance().getXResolution();x++){
-				if(absoluteDataDifference[x][y] >= 0){
-					normalizedRelativeMatrix[x][y] = (double)((double)absoluteDataDifference[x][y] - (double)minPosValue)/((double)maxPosValue - (double)minPosValue);	
+				if(relativeDataDifference[x][y] > maxPosValue){
+					relativeDataDifference[x][y] = maxPosValue;
+				} else if (relativeDataDifference[x][y] < maxNegValue) {
+					relativeDataDifference[x][y] = maxNegValue;
 				} else {
-					normalizedRelativeMatrix[x][y] = (double)((double)absoluteDataDifference[x][y] - (double)minNegValue)/((double)maxNegValue - (double)minNegValue) * (-1.0);
+					if(relativeDataDifference[x][y] >= 0){
+						normalizedRelativeMatrix[x][y] = (double)((double)relativeDataDifference[x][y] - minPosValue)/(maxPosValue - minPosValue);	
+					} else {
+						normalizedRelativeMatrix[x][y] = (double)((double)relativeDataDifference[x][y] - minNegValue)/(maxNegValue - minNegValue) * (-1.0);
+					}
 				}
 			}
 		}
-		//OUTPUT:
+
+		//Create raw Image:
+		ImageController imageController = new ImageController();
+		try {
+			File imageFile = new File("dat/img/rawMap.png");
+			imageFile.createNewFile();
+			imageController.drawHeatmap(normalizedRelativeMatrix);
+			imageController.saveImage(imageFile);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//DEBUG MATRICES OUTPUT:
 		DecimalFormat dFormat = new DecimalFormat("0.00");
 		System.out.println("Present Occurencies:");
 		for(int y=0; y<Gridmodel.getInstance().getYResolution(); y++){
@@ -119,19 +160,8 @@ public class GridController {
 			System.out.println("");
 		}
 		System.out.println("maxNeg="+maxNegValue+", minNeg="+minNegValue+", maxPos="+maxPosValue+", minPos="+minPosValue);
-		//Create Image:
-		ImageController imageController = new ImageController();
-		try {
-			File imageFile = new File("dat/img/rawMap.png");
-			imageFile.createNewFile();
-			imageController.drawHeatmap(normalizedRelativeMatrix);
-			imageController.saveImage(imageFile);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
-	
+
 	private int[][] calculateWeightedAverage(GridModelData[] matrices){ //TODO weight is not considered yet
 		double[][] tempResult = new double[Gridmodel.getInstance().getXResolution()][Gridmodel.getInstance().getYResolution()];
 		int[][] result = new int[Gridmodel.getInstance().getXResolution()][Gridmodel.getInstance().getYResolution()];
