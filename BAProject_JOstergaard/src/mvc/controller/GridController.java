c4a2bbf48db05fd321eb35e7d7a9d94d17d06b33
Jonas Ostergaard <omesostergaard@msn.com>
2016-04-26 17:30:21 +0200

    img folder added to dat, ImageController implemented

--- BAProject_JOstergaard/src/mvc/controller/GridController.java
+++ BAProject_JOstergaard/src/mvc/controller/GridController.java
@@ -1,6 +1,8 @@
 package mvc.controller;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.ArrayList;
@@ -21,8 +23,8 @@ public class GridController {
 	
 	public void analyze(int xResolution, int yResolution, Date fromDate, Date toDate, Coordinate topLeft, Coordinate botRight){
 		Gridmodel.getInstance().init(xResolution, yResolution, topLeft, botRight);
-		long dateInterval = toDate.getTime() - fromDate.getTime(); //TODO intervall hardcoded
-		int intervalAmount = 6; //TODO hardcoded
+		long dateInterval = toDate.getTime() - fromDate.getTime(); //interval is current date selection
+		int intervalAmount = 3; //TODO intervalAmount hardcoded
 		Date fDate = fromDate;
 		Date tDate = toDate;
 		Gridmodel.getInstance().getData().setDataMatrix(this.countGridOccurenciesFromTo(fDate, tDate));
@@ -35,19 +37,52 @@ public class GridController {
 		for(int i=0; i<intervalAmount; i++){
 			fDate = new Date(fromDate.getTime() - dateInterval*i);
 			tDate = new Date(fDate.getTime() + dateInterval);
-			newData = new GridModelData(1.0 - ((1.0 / intervalAmount) * (i+1)));
+			newData = new GridModelData(1.0 - ((1.0 / intervalAmount) * i)); //TODO Weight calculation is linear at the moment
 			newData.setDataMatrix(this.countGridOccurenciesFromTo(fDate, tDate));
 			this.pastGridModelData.add(newData);
 		}
+		int minNegValue = Integer.MIN_VALUE, maxNegValue = Integer.MAX_VALUE, minPosValue = Integer.MAX_VALUE,maxPosValue = Integer.MIN_VALUE;
 		int[][] pastDataAverage = this.calculateWeightedAverage(this.pastGridModelData.toArray(new GridModelData[0]));
-		int[][] dataDifference = new int[Gridmodel.getInstance().getXResolution()][Gridmodel.getInstance().getYResolution()];
+		int[][] absoluteDataDifference = new int[Gridmodel.getInstance().getXResolution()][Gridmodel.getInstance().getYResolution()];
+		double[][] relativeDataDifference = new double[Gridmodel.getInstance().getXResolution()][Gridmodel.getInstance().getYResolution()];
 		for(int y=0; y<Gridmodel.getInstance().getYResolution(); y++){
 			for(int x=0; x<Gridmodel.getInstance().getXResolution();x++){
-				dataDifference[x][y] = Gridmodel.getInstance().getData().getDataMatrix()[x][y] - pastDataAverage[x][y];
+				absoluteDataDifference[x][y] = Gridmodel.getInstance().getData().getDataMatrix()[x][y] - pastDataAverage[x][y];
+				if(absoluteDataDifference[x][y] >= 0){ //Positive:
+					if(absoluteDataDifference[x][y] > maxPosValue){
+						maxPosValue = absoluteDataDifference[x][y];
+					}
+					if(absoluteDataDifference[x][y] < minPosValue){
+						minPosValue = absoluteDataDifference[x][y];
+					}
+				} else { //Negative:
+					if(absoluteDataDifference[x][y] > minNegValue){
+						minNegValue = absoluteDataDifference[x][y];
+					}
+					if(absoluteDataDifference[x][y] < maxNegValue){
+						maxNegValue = absoluteDataDifference[x][y];
+					}
+				}
+				if(absoluteDataDifference[x][y] == 0){
+					relativeDataDifference[x][y] = 0.0;		
+				} else {
+					relativeDataDifference[x][y] = (double)absoluteDataDifference[x][y] / (double)Gridmodel.getInstance().getData().getDataMatrix()[x][y];
+				}
 			}
 			System.out.println("");
 		}
+		double[][] normalizedRelativeMatrix = new double[Gridmodel.getInstance().getXResolution()][Gridmodel.getInstance().getYResolution()];
+		for(int y=0; y<Gridmodel.getInstance().getYResolution(); y++){
+			for(int x=0; x<Gridmodel.getInstance().getXResolution();x++){
+				if(absoluteDataDifference[x][y] >= 0){
+					normalizedRelativeMatrix[x][y] = (double)((double)absoluteDataDifference[x][y] - (double)minPosValue)/((double)maxPosValue - (double)minPosValue);	
+				} else {
+					normalizedRelativeMatrix[x][y] = (double)((double)absoluteDataDifference[x][y] - (double)minNegValue)/((double)maxNegValue - (double)minNegValue) * (-1.0);
+				}
+			}
+		}
 		//OUTPUT:
+		DecimalFormat dFormat = new DecimalFormat("0.00");
 		System.out.println("Present Occurencies:");
 		for(int y=0; y<Gridmodel.getInstance().getYResolution(); y++){
 			for(int x=0; x<Gridmodel.getInstance().getXResolution();x++){
@@ -62,30 +97,61 @@ public class GridController {
 			}
 			System.out.println("");
 		}
-		System.out.println("Difference Occurences:");
+		System.out.println("Absolute Difference Occurences:");
 		for(int y=0; y<Gridmodel.getInstance().getYResolution(); y++){
 			for(int x=0; x<Gridmodel.getInstance().getXResolution();x++){
-				System.out.print(" "+dataDifference[x][y]+" ");
+				System.out.print(" "+absoluteDataDifference[x][y]+" ");
 			}
 			System.out.println("");
+		}
+		System.out.println("Relative Difference Occurences:");
+		for(int y=0; y<Gridmodel.getInstance().getYResolution(); y++){
+			for(int x=0; x<Gridmodel.getInstance().getXResolution();x++){
+				System.out.print(" "+Math.round(relativeDataDifference[x][y]*100)+"% ");
+			}
+			System.out.println("");
+		}
+		System.out.println("Normalized Relative Differences:");
+		for(int y=0; y<Gridmodel.getInstance().getYResolution(); y++){
+			for(int x=0; x<Gridmodel.getInstance().getXResolution();x++){
+				System.out.print(" "+normalizedRelativeMatrix[x][y]+" ");
+			}
+			System.out.println("");
+		}
+		System.out.println("maxNeg="+maxNegValue+", minNeg="+minNegValue+", maxPos="+maxPosValue+", minPos="+minPosValue);
+		//Create Image:
+		ImageController imageController = new ImageController();
+		try {
+			File imageFile = new File("dat/img/rawMap.png");
+			imageFile.createNewFile();
+			imageController.drawHeatmap(normalizedRelativeMatrix);
+			imageController.saveImage(imageFile);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 	
 	private int[][] calculateWeightedAverage(GridModelData[] matrices){ //TODO weight is not considered yet
 		double[][] tempResult = new double[Gridmodel.getInstance().getXResolution()][Gridmodel.getInstance().getYResolution()];
 		int[][] result = new int[Gridmodel.getInstance().getXResolution()][Gridmodel.getInstance().getYResolution()];
+		double divider = 0.0;
+		//sum all weights for the divider:
+		for(GridModelData gMD : matrices){
+			divider += gMD.getWeight();
+		}
 		//sum all matrices up modified by each weight:
 		for(int i=0; i<matrices.length; i++){
 			for(int x=0; x<Gridmodel.getInstance().getXResolution(); x++){
 				for(int y=0; y<Gridmodel.getInstance().getYResolution(); y++){
-					tempResult[x][y] += (double)matrices[i].getDataMatrix()[x][y];
+					tempResult[x][y] += (double)matrices[i].getDataMatrix()[x][y] * matrices[i].getWeight();
 				}
 			}
 		}
 		//divide by the amount of matrices:
 		for(int x=0; x<Gridmodel.getInstance().getXResolution(); x++){
 			for(int y=0; y<Gridmodel.getInstance().getYResolution(); y++){
-				result[x][y] = (int)(tempResult[x][y] / matrices.length);
+				result[x][y] = (int)(tempResult[x][y] / divider);
 			}
 		}
 		return result;
---
+++ BAProject_JOstergaard/src/mvc/controller/ImageController.java
@@ -0,0 +1,61 @@
+package mvc.controller;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class ImageController {
+	private BufferedImage currentImage;
+	
+	public ImageController(){
+		
+	}
+	
+	/**
+	 * 
+	 * @param matrix with values between -1.0(BLUE) and 1.0(RED)
+	 * @return
+	 */
+	public void drawHeatmap(double[][] matrix){
+		this.currentImage = new BufferedImage(matrix.length, matrix[0].length, BufferedImage.TYPE_INT_ARGB);
+		int rgb,white = new Color(0,0,0,0).getRGB();
+		for(int y=0; y<matrix.length; y++){
+			for(int x=0; x<matrix[0].length; x++){
+				if(matrix[x][y] > 0){
+					rgb = new Color(255,0,0,(int)(matrix[x][y]*255)).getRGB();
+				} else if(matrix[x][y] == 0){
+					rgb = white;
+				} else {
+					rgb = new Color(0,0,255,(int)(Math.abs(matrix[x][y])*255)).getRGB();
+				}
+				this.currentImage.setRGB(x, y, rgb);
+			}
+		}
+	}
+	
+	public ImageController(File inputImage) throws IOException{
+		this.loadImage(inputImage);
+	}
+	
+	public void loadImage(File inputImage) throws IOException{
+		currentImage = ImageIO.read(inputImage);
+	}
+	
+	public void saveImage(File outputImage) throws IOException{
+		ImageIO.write(this.currentImage, "png", outputImage);
+	}
+
+	public BufferedImage getCurrentImage() {
+		return currentImage;
+	}
+
+	public void setCurrentImage(BufferedImage currentImage) {
+		this.currentImage = currentImage;
+	}
+}
